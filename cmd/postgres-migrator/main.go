883549945c5446a517f482e3a7468ed54ae1c1ee
crisog <ortega.cpp@gmail.com>
2025-10-03 08:53:41 -0400

    feat: add CLI application in cmd/postgres-migrator

---
+++ cmd/postgres-migrator/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/crisog/postgres-migrator/internal/config"
+	"github.com/crisog/postgres-migrator/pkg/migration"
+)
+
+func main() {
+	exitCode := run()
+	os.Exit(exitCode)
+}
+
+func run() int {
+	cfg, err := config.LoadFromEnv()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Configuration error: %v\n", err)
+		return 1
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		sig := <-sigChan
+		log.Printf("\nReceived signal: %v. Cancelling operation...\n", sig)
+		cancel()
+	}()
+
+	logger := log.New(os.Stdout, "", log.Ltime)
+	if err := migration.Run(ctx, cfg, logger); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		return 1
+	}
+
+	return 0
+}
